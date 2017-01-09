27480b43fc8146470c0225c0189f4d7883e2656f
Dušan Miletić <d@miletich.cc>
2017-01-09 09:50:14 +0100

    Add queue

---
+++ queue.js
@@ -0,0 +1,38 @@
+"use strict";
+
+function queue(){
+  const items = [];
+
+  function enqueue(item){
+    return items.push(item);
+  }
+
+  function dequeue(){
+    return items.shift();
+  }
+
+  function front(){
+    return items[0];
+  }
+
+  function size(){
+    return items.length;
+  }
+
+  function isEmpty(){
+    return items.length === 0;
+  }
+
+  function clear(){
+    items = [];
+    return true;
+  }
+
+  function toString(){
+    return items.toString();
+  }
+
+  return { enqueue, dequeue, front, size, isEmpty, clear, toString };
+}
+
+default export queue;
