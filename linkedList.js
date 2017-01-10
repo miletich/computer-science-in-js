ec09f65977a6a6a5e14ce300cbea4592ca722d98
Dušan Miletić <d@miletich.cc>
2017-01-10 15:36:13 +0100

    Add linked list outline

---
+++ linkedList.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function linkedList(){
+  let head = null;
+
+  function node(value){
+    let next = null;
+    return { value, next };
+  }
+
+  function append(value){}
+
+  function insert(position, value){}
+
+  function removeAt(position){}
+
+  function remove(value){}
+
+  function getHead(){}
+
+  function indexOf(){}
+
+  function size()
+
+  function isEmpty(){}
+
+  function clear(){}
+
+  function toString(){}
+}
+
+export default linkedList;
