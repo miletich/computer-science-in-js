f283066dbc0e0cfc3b0b8b61a0c1e2adf09d9890
Dušan Miletić <d@miletich.cc>
2017-01-11 12:20:01 +0100

    Initialise circular doubl linked list from doubly linked list

---
+++ circularDoublyLinkedList.js
@@ -0,0 +1,158 @@
+"use strict";
+
+function circularDoublyLinkedList(){
+  let head = null,
+    tail = null;
+
+  function node(value){
+    let next = null,
+      previous = null;
+    return { value, next, previous };
+  }
+
+  function append(value){
+    const newNode = node(value);
+
+    if (head === null) {
+      head = newNode;
+      tail = newNode;
+    } else {
+      tail.next = newNode;
+      newNode.previous = tail;
+      tail = newNode;
+    }
+  }
+
+  function insert(position, value){
+    if (position >= 0 && position < size()) {
+      const newNode = node(value);
+      let current = head,
+        previous,
+        index = 0;
+
+      if (position === 0) {
+
+        if (head === null) {
+          head = newNode;
+          tail = newNode;
+        } else {
+          head = newNode;
+          current.previous = newNode;
+          head.next = current;
+        }
+
+      } else if (position === size() -1) {
+
+        current = tail;
+        current.next = newNode;
+        tail = newNode;
+        tail.previous = current;
+
+      } else {
+
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = newNode;
+        newNode.previous = previous;
+        newNode.next = current;
+        current.previous = newNode;
+      }
+
+      return true;
+
+    } else {
+      return false;
+    }
+  }
+
+  function removeAt(position){
+    if (position >= 0 && position < size()) {
+      let current = head,
+        previous,
+        index = 0;
+
+      if (position === 0) {
+        if (size() === 1) {
+          head = null;
+          tail = null;
+        } else {
+          head = head.next;
+          head.previous = null;
+        }
+      } else if (position === size() -1) {
+        current = tail;
+        tail = current.previous;
+        tail.next = null;
+
+      } else {
+
+        while (index < position) {
+          index++;
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+        current.next.previous = previous;
+      }
+
+      return current.value;
+
+    } else {
+      return null;
+    }
+  }
+
+  function remove(value){
+    return removeAt(indexOf(value));
+  }
+
+  function getHead(){
+    return head;
+  }
+
+  function getTail(){
+    return tail;
+  }
+
+  function indexOf(value){
+    return function position(current = head, count = 0){
+      return current === null
+        ? null
+        : value === current.value
+          ? count
+          : position(current.next, ++count);
+    }();
+  }
+
+  function size(){
+    return function length(current = head, count = 0){
+      return current === null
+        ? count
+        : length(current.next, ++count);
+    }();
+  }
+
+  function isEmpty(){
+    return size() === 0;
+  }
+
+  function clear(){
+    head = null;
+    tail = null;
+    return true;
+  }
+
+  function toString(){
+    return function makeString(current = head, str = ""){
+      return current === null
+        ? str
+        : makeString(current.next, str + current.value + "\n");
+    }();
+  }
+
+  return { append, insert, removeAt, remove, getHead, getTail, indexOf, size, isEmpty, clear, toString };
+}
+
+export default circularDoublyLinkedList;
