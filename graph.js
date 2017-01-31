0ab12c54c0dc01d29371872b1c4d81b647d93779
Dušan Miletić <d@miletich.cc>
2017-01-31 12:47:33 +0100

    Initialise graph data structure with basic methods

---
+++ graph.js
@@ -0,0 +1,38 @@
+"use strict";
+import dictionary from "./dictionary";
+
+function graph() {
+  const vertices = [];
+  const adjList = dictionary();
+
+  function addVertex(v) {
+    vertices.push(v);
+    adjList.set(v, []);
+  }
+
+  function addEdge(v, w) {
+    adjList.get(v).push(w); // Would be enough for a directed graph
+    adjList.get(w).push(v); // Needed for an undirected graph
+  }
+
+  function toString() {
+    let s = "";
+
+    for (let i = 0; i < vertices.length; i++) {
+      s += vertices[i] + " -> ";
+      const neighbours = adjList.get(vertices[i]);
+
+      for (let i = 0; i < neighbours.length; i++) {
+        s += neighbours[i] + " ";
+      }
+
+      s += "\n";
+    }
+
+    return s;
+  }
+
+  return { addVertex, addEdge, toString };
+}
+
+export default graph;
