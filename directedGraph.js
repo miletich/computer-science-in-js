fb94399b1539213e318c0b2343b1dbdb0c27cdbd
Dušan Miletić <d@miletich.cc>
2017-02-02 16:50:20 +0100

    Add directed graph

---
+++ directedGraph.js
@@ -0,0 +1,182 @@
+"use strict";
+import dictionary from "./dictionary";
+import queue from "./queue";
+import stack from "./stack";
+
+function graph() {
+  const vertices = [];
+  const adjList = dictionary();
+
+  function addVertex(v) {
+    vertices.push(v);
+    adjList.set(v, []);
+  }
+
+  function addEdge(v, w) {
+    adjList.get(v).push(w);
+  }
+
+  function toString() {
+    let s = "";
+
+    for (let i = 0; i < vertices.length; i++) {
+      const neighbours = adjList.get(vertices[i]);
+      s += vertices[i] + " -> ";
+
+      for (let i = 0; i < neighbours.length; i++) {
+        s += [neighbours[i]] + " ";
+      }
+
+      s += "\n";
+    }
+
+    return s;
+  }
+
+  function bfs(callback, v = vertices[0]){
+    const status = initialiseStatus();
+    const q = queue();
+    q.enqueue(v);
+
+    while (!q.isEmpty()) {
+      const u = q.dequeue();
+      status[u] = "discovered";
+      const neighbours = adjList.get(u);
+
+      for (let i = 0; i < neighbours.length; i++) {
+        const w = neighbours[i];
+
+        if (status[w] === "unvisited") {
+          status[w] = "discovered";
+          q.enqueue(w);
+        }
+      }
+
+      if (callback) {
+        callback(u);
+      }
+
+      status[u] = "explored";
+    }
+
+    function initialiseStatus() {
+      const status = {};
+
+      for (let i = 0; i < vertices.length; i++) {
+        status[vertices[i]] = "unvisited";
+      }
+
+      return status;
+    }
+  }
+
+  function shortestPath(v = vertices[0]) {
+    // Calculate distances and predecessors from 'v' to every other vertext
+    const status = initialise().status;
+    const predecessors = initialise().predecessors;
+    const distance = initialise().distance;
+    const q = queue();
+
+    q.enqueue(v);
+
+    while (!q.isEmpty()) {
+      const u = q.dequeue();
+      const neighbours = adjList.get(u);
+      status[u] = "discovered";
+
+      for (let i = 0; i < neighbours.length; i++) {
+        const w = neighbours[i];
+
+        if (status[w] === "unvisited") {
+          status[w] = "discovered";
+          predecessors[w] = u;
+          distance[w] = distance[u] + 1;
+          q.enqueue(w);
+        }
+      }
+
+      status[u] = "explored";
+    }
+
+    // Display paths from vertex 'v' to every other vertex in the graph
+    const fromVertex = v;
+    let s = "";
+    const path = stack();
+
+    for (let i = 0; i < vertices.length; i++) {
+      if (status[vertices[i]] === "explored") {
+        const toVertex = vertices[i];
+
+        for (let v = toVertex; v !== fromVertex; v = predecessors[v]) {
+          path.push(v);
+        }
+        s += fromVertex;
+        while (!path.isEmpty()) {
+          s += " - " + path.pop();
+        }
+        s += "\n";
+      }
+
+    }
+    return s;
+
+    // Helper function
+    function initialise() {
+      const status = {};
+      const predecessors = {};
+      const distance = {};
+
+      for (let i = 0; i < vertices.length; i++) {
+        status[vertices[i]] = "unvisited";
+        predecessors[vertices[i]] = null;
+        distance[vertices[i]] = 0;
+      }
+
+      return { status, predecessors, distance};
+    }
+  }
+
+  function dfs(callback) {
+    const status = initialiseStatus();
+
+    for (let i = 0; i < vertices.length; i++) {
+      if (status[vertices[i]] === "unvisited") {
+        dfsVisit(vertices[i], status, callback);
+      }
+    }
+
+    // Does the actual visiting
+    function dfsVisit(u, status, callback) {
+      status[u] = "discovered";
+      if (callback) {
+        callback(u);
+      }
+
+      const neighbours = adjList.get(u);
+      for (let i = 0; i < neighbours.length; i++) {
+        const w = neighbours[i];
+        if (status[w] === "unvisited") {
+          dfsVisit(w, status, callback);
+        }
+      }
+
+      status[u] = "explored";
+    }
+
+
+    // Helper function
+    function initialiseStatus() {
+      const status = {};
+
+      for (let i = 0; i < vertices.length; i++) {
+        status[vertices[i]] = "unvisited";
+      }
+
+      return status;
+    }
+  }
+
+  return { addVertex, addEdge, toString, bfs, shortestPath, dfs };
+}
+
+export default graph;
